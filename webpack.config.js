572d9a7784c4ba718fdc81e78242e24025ed9be4
Ajit Kumar <dellevenjack@gmail.com>
2019-09-20 15:03:09 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  mode: "development",
+  entry: {
+    comboBox: './src/comboBox.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'comboBox',
+    libraryExport: 'comboBox',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [{
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+          "postcss-loader"
+        ]
+      }
+    ]
+  }
+};
